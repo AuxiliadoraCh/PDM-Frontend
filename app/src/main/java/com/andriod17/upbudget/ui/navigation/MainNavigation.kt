c9590ba6f3a77457d71368f14548a1254d42bef7
rebecaeta <00225023@uca.edu.sv>
2025-07-04 03:49:27 -0600

    Add ExpenseHistory Navigation

--- app/src/main/java/com/andriod17/upbudget/ui/navigation/MainNavigation.kt
+++ app/src/main/java/com/andriod17/upbudget/ui/navigation/MainNavigation.kt
@@ -30,9 +30,8 @@ fun MainNavigation(navController: NavHostController) {
         composable("settings"){
             SettingsScreen(navController = navController)
         }
-//        composable<ExpenseHistoryNavigation> {
-//            //Probar el view model pero me da error
-////            ExpenseHistoryScreen(viewModel = viewModel, navController = navController)
-//        }
+        composable<ExpenseHistoryNavigation>{
+            ExpenseHistoryScreen(navController = navController)
+        }
     }
 }
--- app/src/main/java/com/andriod17/upbudget/ui/navigation/Screen.kt
+++ app/src/main/java/com/andriod17/upbudget/ui/navigation/Screen.kt
@@ -2,16 +2,4 @@ package com.andriod17.upbudget.ui.navigation
 import kotlinx.serialization.Serializable
 
 @Serializable
-object HomeNavigation
-
-@Serializable
-object PromotionsNavigation
-
-@Serializable
-object ContentNavigation
-
-@Serializable
-object SettingsNavigation
-
-@Serializable
 object ExpenseHistoryNavigation
